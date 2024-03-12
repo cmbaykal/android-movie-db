ac25d26403f45c298060cdc6c0e7d22575056549
cmert <cmertbaykal@gmail.com>
2024-03-12 03:28:24 +0300

    - Layout Revisions

--- app/src/main/java/com/baykal/moviedb/ui/screen/movieDetail/MovieDetailScreen.kt
+++ app/src/main/java/com/baykal/moviedb/ui/screen/movieDetail/MovieDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.baykal.moviedb.ui.screen.movieDetail
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -8,9 +9,14 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -49,12 +55,17 @@ fun MovieDetailScreen(id: Int?) {
                         contentDescription = "image",
                         contentScale = ContentScale.Fit,
                         modifier = Modifier
-                            .height(200.dp)
-                            .fillMaxWidth()
+                            .align(Alignment.CenterHorizontally)
+                            .padding(top = 10.dp)
+                            .height(420.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                            .shadow(elevation = 10.dp, shape = RoundedCornerShape(10.dp))
                     )
                     Text(
                         modifier = Modifier.padding(10.dp),
-                        fontSize = 18.sp,
+                        color = Color.Black,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
                         text = movie.name ?: movie.title ?: movie.originalTitle ?: ""
                     )
                     Row(
@@ -63,11 +74,13 @@ fun MovieDetailScreen(id: Int?) {
                     ) {
                         Text(
                             modifier = Modifier.padding(10.dp),
+                            color = Color.Gray,
                             fontSize = 14.sp,
                             text = buildString { movie.genres?.forEach { append("${it.name} ") } }
                         )
                         Text(
                             modifier = Modifier.padding(10.dp),
+                            color = Color.Gray,
                             fontSize = 14.sp,
                             text = movie.releaseDate ?: ""
                         )
